0cca0983aaabba50fbc98aa1c60a301b49b494cd
QuangHuyDo <quanghuy1607hn@gmail.com>
2022-03-15 11:45:11 +0700

    Update top bar (better image resolution)

--- Assets/Scripts/GameInside/BattleController.cs
+++ Assets/Scripts/GameInside/BattleController.cs
@@ -378,7 +378,7 @@ public class BattleController : MonoBehaviour
             if (cache?.target == null)
                 Debug.Log("<color=red>Missing</color> target, queue not add battle infor");
             else
-                Debug.LogError("Missing something here");
+                //Debug.LogError("Missing something here");
             return;
         }
 
